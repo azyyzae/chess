6bafaade632114c91306858491ae6a9acc1771c3
azyyzae <blankhut@gmail.com>
2025-08-05 16:50:06 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,9 +6,9 @@ if _G.__CHESS_AI_LOADED__FULL then
 end
 _G.__CHESS_AI_LOADED__FULL = true
 
-modules.config = loadstring(game:HttpGet("https://raw.githubusercontent.com/azyyzae/chess/main/config.lua"))()
-modules.state = loadstring(game:HttpGet("https://raw.githubusercontent.com/azyyzae/chess/main/core/state.lua"))()
-modules.ai = loadstring(game:HttpGet("https://raw.githubusercontent.com/azyyzae/chess/main/core/ai.lua"))()
-modules.gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/azyyzae/chess/main/core/gui.lua"))()
+modules.config = loadstring(game:HttpGet("https://raw.githubusercontent.com/Azyyzae/chessBot/main/config.lua"))()
+modules.state = loadstring(game:HttpGet("https://raw.githubusercontent.com/Azyyzae/chessBot/main/core/state.lua"))()
+modules.ai = loadstring(game:HttpGet("https://raw.githubusercontent.com/Azyyzae/chessBot/main/core/ai.lua"))()
+modules.gui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Azyyzae/chessBot/main/core/gui.lua"))()
 
 modules.gui.init(modules)
